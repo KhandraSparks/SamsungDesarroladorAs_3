d72b50eda5dda00d56a3feac17937cf0e04d62db
KhandraSparks <69635236+KhandraSparks@users.noreply.github.com>
2023-03-26 21:41:38 +0100

    Delete Ejemplos TS directory

--- Ejemplos TS/005_Rest.ts
+++
@@ -1,5 +0,0 @@
-var miVariable = (dato1:string, ...restArgumentos:string[]):string => {
-    return `${dato1} ${restArgumentos.join(" ")}`;
-    }
-    
-    console.log(miVariable("Angel", "fas", "fgskjs", "fafhalfhdl"));
