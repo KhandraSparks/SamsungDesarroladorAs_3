5a815914c11a0e992b897459ed9b46c4cf40e5b1
KhandraSparks <naomiaw92@gmail.com>
2023-04-08 03:01:18 +0100

    Started work

---
+++ T3_APIs/main.js
@@ -0,0 +1,6 @@
+//Comprobar que el navegador soporta la función
+if (window.File && window.FileReader && window.FileList) {
+    //aqui mi codigo a ejecutar;
+    } else {
+    alert('La API de FILE no es soportada en este navegador.');
+    }
